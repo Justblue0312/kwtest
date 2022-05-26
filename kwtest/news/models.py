e5d1de3f6272e233aa116829e0b87f7531630758
Justblue0312 <trao0312@gmail.com>
2022-05-26 14:26:33 +0700

    a

--- kwtest/news/models.py
+++ kwtest/news/models.py
@@ -7,16 +7,16 @@ class ArticleNews(models.Model):
     id = models.UUIDField(default=uuid.uuid4, unique=True,
                           primary_key=True, editable=False)
     created_at = models.DateTimeField(auto_now_add=True)
-    title = models.CharField(max_length=255, null=True, blank=True)
-    abstract = models.CharField(max_length=10000, blank=True, null=True)
+    title = models.CharField(max_length=10000, null=True, blank=True)
+    abstract = models.CharField(max_length=100000, blank=True, null=True)
     url = models.URLField()
     content = models.TextField(null=True, blank=True)
     publish_date = models.DateTimeField(blank=True, null=True)
     image_url = models.URLField()
     attribution = models.CharField(max_length=255, null=True, blank=True)
-    image_name = models.CharField(max_length=5000, null=True, blank=True)
-    image_caption = models.CharField(max_length=5000, null=True, blank=True)
-    source = models.CharField(max_length=255, null=True, blank=True)
+    image_name = models.CharField(max_length=10000, null=True, blank=True)
+    image_caption = models.CharField(max_length=10000, null=True, blank=True)
+    source = models.CharField(max_length=10000, null=True, blank=True)
     reaction_count = models.IntegerField(default=0)
 
     def __str__(self):
