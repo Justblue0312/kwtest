075dbabc64a3097c01f37058ee971344f28c7de1
Bùi Công Trào <71886798+Justblue0312@users.noreply.github.com>
2022-05-26 11:50:27 +0700

    Update settings.py

--- kwtest/keyword_proj/settings.py
+++ kwtest/keyword_proj/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--3&ti&!*x()*p#d2=1gomffx-1x&2+pxre&2(0=e(v7vf)!=vs'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com']
 
